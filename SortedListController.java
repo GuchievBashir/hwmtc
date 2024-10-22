eaa51df383ed88fe8b8c27788ef4543dc28e700f
Guchiev Magomed-Bashir <magomedbashir095@gmail.com>
2024-10-22 20:35:42 +0300

    Create directory for test

--- SortedListController.java
+++ SortedListController.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 
-public class SortingManager {
+public class SortedListController {
 
   private final List<SortingInterface> sorts = new ArrayList<>();
 
